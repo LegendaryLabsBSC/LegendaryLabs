3814c1f568835b4a423ba991635a1add5b4fa5e4
gavinspr <gavin.sproles@gmail.com>
2021-10-09 14:48:48 -0400

    add hardhat plugins

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,12 @@
 require("@nomiclabs/hardhat-waffle");
 require('dotenv').config()
 require('hardhat-contract-sizer');
+require("hardhat-gas-reporter");// need to configure when test are set up
+require("@nomiclabs/hardhat-solhint");
+require("hardhat-tracer") // need to configure when test are set up
+require('hardhat-spdx-license-identifier');
+require('hardhat-docgen');
+require('hardhat-storage-layout');
 
 const PRIVATEKEY = process.env.PRIVATEKEY
 
@@ -25,6 +31,15 @@ module.exports = {
   paths: {
     artifacts: './frontend/src/artifacts'
   },
+  gasReporter: {
+    currency: 'BSC',
+    gasPrice: 10,
+    enabled: (process.env.REPORT_GAS) ? true : false
+  },
+  spdxLicenseIdentifier: {
+    overwrite: true,
+    runOnCompile: true,
+  },
   networks: {
     hardhat: {
       chainId: 1337
@@ -41,5 +56,10 @@ module.exports = {
     //   url: "https://apis-sj.ankr.com/3993586e443942098e59d5b71e4a7e09/7eb7caa0231f32a5cc5bbcb5dbeab631/binance/full/test",
     //   accounts: [`0x${PRIVATEKEY}`]
     // }
+  },
+  docgen: {
+    path: './docs',
+    clear: true,
+    runOnCompile: true,
   }
 };
