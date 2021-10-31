08d430af4e110511f8db6ad66ae9a1a0f1b87059
Spencer Pope <spope@rocelec.com>
2021-10-31 15:22:03 -0400

    coming soon

---
+++ frontend/src/components/coming-soon/coming-soon.js
@@ -0,0 +1,24 @@
+import React from 'react'
+
+function ComingSoon() {
+  return (
+    <div className="container">
+        <div className="text-center">
+            <h2 className="nk-title display-4">Coming Soon</h2>
+            <div className="nk-title-sep-icon">
+                <span className="icon"><span className="ion-clock"/></span>
+            </div>
+            <div className="nk-gap-2"/>
+            <div className="row">
+                <div className="col-md-8 offset-md-2">
+                    <p className="lead">It&apos;s gonna be Legendary...</p>
+                    <div className="nk-gap-2"/>
+                    {/* <div className="nk-countdown" data-end="2021-11-26 14:30" data-timezone="EST"></div> */}
+                </div>
+            </div>
+        </div>
+    </div>
+  );
+}
+
+export default ComingSoon;
