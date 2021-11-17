66f1ac35efb3495e0fc6bb3ef10025d7db8a9b30
gavinspr <gavin.sproles@gmail.com>
2021-11-16 22:12:49 -0500

    remove blzd test env

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,91 +1,26 @@
-import React, { useEffect, Suspense, lazy } from 'react'
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
-import { useWallet } from '@binance-chain/bsc-use-wallet'
-import { ResetCSS } from '@legendarylabs/uikit'
-import BigNumber from 'bignumber.js'
-import { useFetchPriceList, useFetchPublicData } from 'state/hooks'
-import useGetDocumentTitlePrice from './hooks/useGetDocumentTitlePrice'
-import GlobalStyle from './style/Global'
-import Menu from './components/Menu'
-import PageLoader from './components/PageLoader'
-import Pools from './views/Pools'
-import NftGlobalNotification from './views/Nft/components/NftGlobalNotification'
-import XBLZD from './views/XBLZD'
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
 
-// Route-based code splitting
-// Only pool is included in the main bundle because of it's the most visited page'
-const Home = lazy(() => import('./views/Home'))
-const Farms = lazy(() => import('./views/Farms'))
-const Lottery = lazy(() => import('./views/Lottery'))
-// const Pools = lazy(() => import('./views/Pools'))
-// const Ifos = lazy(() => import('./views/Ifos'))
-const NotFound = lazy(() => import('./views/NotFound'))
-// const Nft = lazy(() => import('./views/Nft'))
-
-// This config is required for number formatting
-// BigNumber.config({
-//   EXPONENTIAL_AT: 1000,
-//   DECIMAL_PLACES: 80,
-// })
-
-const App: React.FC = () => {
-  const { account, connect } = useWallet()
-  useEffect(() => {
-    if (!account && window.localStorage.getItem('connectorId')) {
-      connect('injected')
-    }
-  }, [account, connect])
-
-  useFetchPublicData()
-  useFetchPriceList()
-  useGetDocumentTitlePrice()
-
+function App() {
   return (
-    <Router>
-      <ResetCSS />
-      <GlobalStyle />
-      <Menu>
-        <Suspense fallback={<PageLoader />}>
-          <Switch>
-            <Route path="/" exact>
-              <Home />{' '}
-            </Route>
-            <Route path="/farms">
-              <Farms />
-            </Route>
-            <Route path="/caves">
-              <Lottery />
-            </Route>
-            <Route path="/pools">
-              <Pools />
-            </Route>
-            <Route path="/xBLZD">
-              <XBLZD />
-            </Route>
-            {/* <Route path="/lottery"> */}
-            {/*  <Lottery /> */}
-            {/* </Route> */}
-            {/* <Route path="/ifo"> */}
-            {/*  <Ifos /> */}
-            {/* </Route> */}
-            {/* <Route path="/nft"> */}
-            {/*  <Nft /> */}
-            {/* </Route> */}
-            {/* Redirect */}
-            {/* <Route path="/staking"> */}
-            {/*  <Redirect to="/pools" /> */}
-            {/* </Route> */}
-            {/* <Route path="/syrup"> */}
-            {/*  <Redirect to="/pools" /> */}
-            {/* </Route> */}
-            {/* 404 */}
-            <Route component={NotFound} />
-          </Switch>
-        </Suspense>
-      </Menu>
-      <NftGlobalNotification />
-    </Router>
-  )
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 }
 
-export default React.memo(App)
+export default App;
--- frontend/src/contract_config/contract-config.ts
+++
@@ -1,8 +0,0 @@
-const legendsLaboratory: string = '0x64EbdCd052861332F9E806595471bfA6c0F03110'
-const legendsNFT: string = '0x85bd6Eee5175b1a10698881d93857196C35c1EDd'
-const legendToken: string = '0x7139fBA84eee0b64a98331d29386427E78A42686'
-const legendRejuvenation: string = '0xE85299B780Db9f868E8AF53aC84980609892c60f'
-const legendsMarketplace: string = '0x4Cc7b533e839C11ce96d9f7efc27BdB42f77C93E'
-const legendsMatchingBoard: string = '0xD39cb3B2F0Fe4C7Ae008cb9049bAEbF3F43d0F67'
-
-export { legendsLaboratory, legendsNFT, legendToken, legendRejuvenation, legendsMarketplace, legendsMatchingBoard }
