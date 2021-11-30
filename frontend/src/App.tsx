f39ff845921dbad15cbb74c57d3e3c8ae4d547ae
gavinspr <gavin.sproles@gmail.com>
2021-11-30 01:29:07 -0500

    extract backdrap

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,15 +1,15 @@
-import React, { useState, useEffect, lazy } from "react";
-import { Heading, Flex, Spacer, HStack } from "@chakra-ui/react";
+import React, { useState} from "react";
+import { Flex} from "@chakra-ui/react";
 import Sidebar from "./components/Sidebar/Sidebar";
 import ContractTesting from "./pages/ContractTesting/ContractTesting";
 import {
   BrowserRouter as Router,
   Routes,
-  Route,
-  Link as RouteLink,
+  Route
 } from "react-router-dom";
 import Home from "./pages/Home/Home";
 import Settings from "./pages/Settings/Settings";
+import BackDrop from "./components/BackDrop/BackDrop";
 
 const App = () => {
   const [navSize, changeNavSize] = useState("large");
@@ -18,20 +18,7 @@ const App = () => {
     <Flex flexDirection="row" justifyContent="space-between">
       <Router>
         <Sidebar changeNavSize={changeNavSize} navSize={navSize} />
-        <Flex
-          // pos="sticky"
-          // left="50"
-          mr={navSize === "small" ? "3%" : "4%"}
-          h="95vh"
-          marginTop="2.5vh"
-          boxShadow="0 4px 12px rgba(0,0,0,1)"
-          borderRadius={"30px"}
-          w={navSize === "small" ? "87vw" : "80vw"}
-          flexDir="row"
-          justifyContent="space-between"
-          background="blue.500"
-          // alignItems="center"
-        >
+        <BackDrop navsize={navSize}>
           <Routes>
             <Route path="/" element={<Home navSize={navSize} />} />
             <Route
@@ -40,7 +27,7 @@ const App = () => {
             />
             <Route path="/settings" element={<Settings navSize={navSize} />} />
           </Routes>
-        </Flex>
+        </BackDrop>
       </Router>
     </Flex>
   );
