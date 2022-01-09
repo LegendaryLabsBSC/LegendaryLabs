b3b72da1bf05507140bf87c27d16eaaa9e48d4b5
Spencer Pope <spope@blockchains.com>
2022-01-09 12:18:34 -0500

    remove contract dependent features that cloudflare cant build

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,14 +3,14 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import { useWallet } from '@binance-chain/bsc-use-wallet'
 // import { ResetCSS } from '@legendarylabs/uikit'
 import BigNumber from 'bignumber.js'
-import AllLegends from 'components/all-legends/all-legends'
+// import AllLegends from 'components/all-legends/all-legends'
 // import Menu from './components/Menu'
 // import NftGlobalNotification from './views/Nft/components/NftGlobalNotification'
 import Team from 'components/team/team'
 import Home from 'components/home/home'
 import ComingSoon from './components/coming-soon/coming-soon'
 // import Menu from './components/Menu'
-import FarmsApp from './views/Farms'
+// import FarmsApp from './views/Farms'
 
 const App: React.FC = () => {
 
@@ -37,10 +37,6 @@ const App: React.FC = () => {
         </Route>
         {/* Use FarmsApp for contract testing. import on line 9 */}
         {/* for testing on dev */}
-        <Route path="/contracts" exact>
-          {/* <Home /> */}
-          <FarmsApp />
-        </Route>
       </Switch>
       {/* </Menu> */}
     </Router>
