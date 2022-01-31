5e283675ced2b4de63c5f08b96d0502e66ae0193
Spencer Pope <spope@blockchains.com>
2022-01-30 20:36:18 -0700

    deploy dapp without contract files

--- frontend/src/components/apollo-client/apollo-client.tsx
+++
@@ -1,8 +0,0 @@
-import { ApolloClient, InMemoryCache } from '@apollo/client'
-
-const client = new ApolloClient({
-  uri: 'http://api.dev.legendarylabs.net/graphql',
-  cache: new InMemoryCache(),
-})
-
-export default client
